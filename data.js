bddf39842b416dcb03f846da9e756b37085a53f0
Bruno Simoni <84brunosimoni@gmail.com>
2022-07-16 09:11:53 +0900

    Add gameplay

--- data.js
+++ data.js
@@ -1,15 +1,32 @@
+
 const characterData = {
     hero: {
         name: "Wizard",
         avatar: "images/caglar-canbay-wizard-unsplash.jpg",
         health: 60,
-        diceCount: 3
+        diceCount: 3,
+        currentDiceScore: []
     },
-    monster: {
+    orc: {
         name: "Orc",
         avatar: "images/michaela-kadlecova-orc-unsplash.jpg",
-        health: 10,
-        diceCount: 1
+        health: 30,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "images/vladyslav-kuznietsov-demon-unsplash.jpg",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "images/goblin-pixabay.jpg",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
     }
 }
 
